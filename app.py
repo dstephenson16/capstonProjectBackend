7c5404a5ca55f0e8e08b0457d22589469840aa4a
dstephenson16 <che.dami@gmail.com>
2019-12-18 16:36:43 -0700

    change mailserver URL

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 app = Flask(__name__)
 
 mail_settings = {
-    "MAIL_SERVER": "smtp.gmail.com",
+    "MAIL_SERVER": "smtp.googlemail.com",
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
